5c81ea28ed7d02aa5922bfdae1657eaff82c03f2
corey carter <coreyc538@gmail.com>
2024-10-19 23:53:18 -0400

    starting the data mods now

--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+
+const isOpen = ref(false)
 </script>
 
 <template>
   <nav class="navbar is-info" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
-
         <a
           role="button"
           class="navbar-burger"
           aria-label="menu"
           aria-expanded="false"
-          data-target="navbarBasicExample"
+          :class="{ 'is-active': isOpen }"
+          @click="isOpen = !isOpen"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -22,18 +24,16 @@ import { RouterLink } from 'vue-router'
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-menu" :class="{ 'is-active': isOpen }">
         <div class="navbar-start">
-          <RouterLink to="/" class="navbar-item">Home</RouterLink>
-          <RouterLink to="/about" class="navbar-item">About</RouterLink>
-
+          <RouterLink to="/Index" class="navbar-item">Home</RouterLink>
           <div class="navbar-item has-dropdown is-hoverable">
             <a class="navbar-link"> More </a>
 
             <div class="navbar-dropdown">
-              <a class="navbar-item"> About </a>
-              <a class="navbar-item is-selected"> Jobs </a>
-              <a class="navbar-item"> Contact </a>
+              <RouterLink to="/about" class="navbar-item"> About </RouterLink>
+              <RouterLink to="/jobs" class="navbar-item"> Jobs </RouterLink>
+              <RouterLink to="/contact" class="navbar-item"> Contact </RouterLink>
               <hr class="navbar-divider" />
               <a class="navbar-item"> Report an issue </a>
             </div>
@@ -55,4 +55,9 @@ import { RouterLink } from 'vue-router'
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.router-link-active {
+  font-weight: bold;
+  border-bottom: 2px solid blue;
+}
+</style>
