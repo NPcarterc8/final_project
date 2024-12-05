3d5b3af97a8ae018f050d4a6fca153a3d40ed030
corey carter <coreyc538@gmail.com>
2024-12-04 20:44:54 -0500

    added comments

---
+++ Client/src/models/comments.ts
@@ -0,0 +1,32 @@
+import type { DataEnvelope, DataListEnvelope } from './dataEnvelope'
+import { api } from './myFetch'
+
+export async function getAll() {
+  return api<DataListEnvelope<Comment>>('comments')
+}
+
+export async function getById(id: number) {
+  return api<DataEnvelope<Comment>>(`comments/${id}`)
+}
+
+export function create(comment: Comment) {
+  return api<DataEnvelope<Comment>>('comments', comment)
+}
+
+export function update(comment: Comment) {
+  return api<DataEnvelope<Comment>>(`comments/${comment.id}`, comment, 'PATCH')
+}
+
+export function remove(id: number) {
+  return api<DataEnvelope<Comment>>(`comments/${id}`, undefined, 'DELETE')
+}
+
+export type Comment = {
+  id: number
+  content: string
+  date: Date
+  time: Date
+  location: string
+  workoutType: string
+  userId: number
+}
